d3545af28ede21ec3d60c81109ce5d3832993819
Tyman3413 <tyman3413@gmail.com>
2022-12-26 00:17:11 +0300

    Sprint_3

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 require('./db/connect');
 const Register = require('./models/registers');
+const { restart } = require('nodemon');
 
 const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, '../public');
 const template_path = path.join(__dirname, '../templates/views');
 const partials_path = path.join(__dirname, '../templates/partials');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
 app.set('view engine', 'hbs');
@@ -20,12 +25,68 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/register', (req, res) => {
+app.get('/main', (req, res) => {
+    res.render('index');
+});
+
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
+app.post('/main', async (req, res) => {
     try {
-        console.log(req.body.firstname);
-        res.send(req.body.firstname);
+        const registration_password = req.body.registration_password;
+        const registration_confirm_password =
+            req.body.registration_confirmPassword;
+        const registration_hashpassword = await bcrypt.hash(
+            registration_password,
+            10
+        );
+        const registration_hash_confirm_password = await bcrypt.hash(
+            registration_confirm_password,
+            10
+        );
+
+        const registration_email = req.body.registration_email;
+        const registartion_email_database = await Register.findOne({
+            email: registration_email,
+        });
+
+        if (await Register.findOne({ email: registration_email })) {
+            res.send('Такой пользователь уже зарегистрирован');
+        } else {
+            const registerUser = new Register({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.registration_email,
+                password: registration_hashpassword,
+                confirmPassword: registration_hash_confirm_password,
+            });
+
+            const registeredUser = await registerUser.save();
+            res.status(201).render('test');
+        }
     } catch (error) {
-        res.status(400).send(error);
+        console.log(error);
+        const login_email = req.body.login_email;
+        const login_password = req.body.login_password;
+
+        const login_user_email = await Register.findOne({ email: login_email });
+
+        try {
+            const login_password_match = await bcrypt.compare(
+                login_password,
+                login_user_email.password
+            );
+
+            if (login_password_match === true) {
+                res.status(201).render('test');
+            } else {
+                res.send('Пароль неверный');
+            }
+        } catch (error2) {
+            res.status(400).send('Last error');
+        }
     }
 });
 
